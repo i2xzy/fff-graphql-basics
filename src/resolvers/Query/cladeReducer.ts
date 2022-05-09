b85cf370743c5ae983c8b34e205c1222d58bf480
Isaac Raskin <yitzchokr@live.com>
2022-05-09 13:25:38 +0100

    refactor to ts and return Node type

---
+++ src/resolvers/Query/cladeReducer.ts
@@ -0,0 +1,67 @@
+import { uniq, uniqBy } from 'lodash';
+
+type Node = {
+  [key: string]: { value: string };
+};
+
+type Clade = {
+  name?: string;
+  children?: Clade[];
+  attributes: {
+    id: string;
+    hasChildren?: boolean;
+    parentId?: string;
+    characteristics?: { name: string; value: boolean }[];
+    source?: string[];
+  };
+};
+
+export const cladeReducer = async (
+  clade: Node[],
+  getNode: (id?: string) => Promise<Node[]>,
+  depth = 3
+): Promise<Clade> => {
+  const result = clade.reduce((acc, item) => {
+    const children =
+      item.children && depth > 1
+        ? uniqBy(
+            [
+              ...(acc.children || []),
+              { attributes: { id: item.children.value } },
+            ],
+            'attributes.id'
+          )
+        : [];
+
+    return {
+      ...acc,
+      id: item.id.value,
+      name: item.name.value,
+      children,
+      attributes: {
+        ...acc.attributes,
+        id: item.id.value,
+        hasChildren: item.hasChildren.value === 'true',
+        parentId: item.parentId.value,
+        attributes: [
+          { name: 'isFlying', value: item.isFlying.value === 'true' },
+        ],
+        rank: item.rank.value,
+        source: item.source
+          ? uniq([...(acc.attributes.source || []), item.source.value])
+          : [],
+      },
+    };
+  }, {} as Clade);
+
+  return {
+    ...result,
+    children: await Promise.all(
+      result.children.map(async child => {
+        const node = await getNode(child.attributes.id);
+        const clade = await cladeReducer(node, getNode, depth - 1);
+        return clade;
+      })
+    ),
+  };
+};
--- src/types/Query.ts
+++ src/types/Query.ts
@@ -2,13 +2,14 @@ import { gql } from 'apollo-server';
 
 export default gql`
   type Query {
-    tree(id: ID, depth: Int): Clade
+    tree(id: ID, depth: Int): Node
     clade(id: ID!): Clade
     search(value: String!): [SearchResult]
     mrca(clade1: String!, clade2: String!): MrcaResult
 
     # sparql
-    tree2(id: ID, depth: Int): Clade
+    tree2(id: ID, depth: Int): Node
+    clade2(id: ID!): Clade
     search2(value: String!): [SearchResult]
     mrca2(clade1: String!, clade2: String!): MrcaResult
   }
